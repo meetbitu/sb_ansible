aa6bcecbe15a8c6719746d081e7bb1f050f6300f
Colin Sagan <careof@colinsagan.com>
2020-04-28 11:20:18 +0800

    Fix permissions issues in ansible playbook. Create dummy production config to avoid error. Add floating ip to terraform so it can be automatically associated with droplet.

--- terraform.tf
+++ terraform.tf
@@ -9,6 +9,11 @@ data "digitalocean_ssh_key" "colin" {
   name = "Colin MacBook Pro (15-inch, 2018)"
 }
 
+# When wiping the server and rebuilding don't remove the floating IP, just the assignment below
+resource "digitalocean_floating_ip" "sb_api_ip" {
+  region = "sgp1"
+}
+
 resource "digitalocean_droplet" "sb_api_01" {
   image  = "ubuntu-18-04-x64"
   name   = "sb-api-01"
@@ -16,5 +21,9 @@ resource "digitalocean_droplet" "sb_api_01" {
   size   = "s-1vcpu-1gb"
   monitoring = true
   ssh_keys = [data.digitalocean_ssh_key.colin.id]
-  count = 1
+}
+
+resource "digitalocean_floating_ip_assignment" "sb_api_ip_assingment" {
+  ip_address = digitalocean_floating_ip.sb_api_ip.ip_address
+  droplet_id = digitalocean_droplet.sb_api_01.id
 }
